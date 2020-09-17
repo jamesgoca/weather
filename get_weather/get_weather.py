e241cec0b71df8196dd8cc8c549072b60bb8cc9e
James Gallagher <jamesg@jamesg.app>
2020-09-17 12:08:56 +0100

    feat: Use full file paths for weather retrieving

--- get_weather/get_weather.py
+++ get_weather/get_weather.py
@@ -11,7 +11,7 @@ today = datetime.datetime.now()
 
 # Read data
 
-with open ("data/data.json", "r") as file:
+with open ("/home/james/weather/get_weather/data/data.json", "r") as file:
 	data = json.load(file)
 
 # Add data to JSON data file
@@ -27,5 +27,5 @@ data.append(
 
 # Write new data to file
 
-with open("data/data.json", "w") as new_file:
+with open("/home/james/weather/get_weather/data/data.json", "w") as new_file:
 	json.dump(data, new_file)
