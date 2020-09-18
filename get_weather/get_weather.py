2f7ca88d79ed07ccb12b2f184e8049b7644f5572
James Gallagher <jamesg@jamesg.app>
2020-09-18 11:42:46 +0100

    feat: Fix list multiplication

--- get_weather/get_weather.py
+++ get_weather/get_weather.py
@@ -32,18 +32,18 @@ data.append(
 with open("/home/james/weather/get_weather/data/data.json", "w") as new_file:
 	json.dump(data, new_file)
 
-orange = (255,69,0)
-yellow = (255,255,0)
-blue = 	(0,0,255)
-dark_blue = (0,0,139)
+orange = [255,69,0]
+yellow = [255,255,0]
+blue = 	[0,0,255]
+dark_blue = [0,0,139]
 
 if temp <= 2:
-	color_array = [dark_blue * 64]
+	color_array = dark_blue * 64
 elif temp >= 3 and temp <= 8:
-	color_array = [blue * 64]
+	color_array = blue * 64
 elif temp >= 9 and temp <= 15:
-	color_array = [yellow * 64]
+	color_array = yellow * 64
 else:
-	color_array = [orange * 64]
+	color_array = orange * 64
 
 sense.set_pixels(color_array)
